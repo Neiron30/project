93256610eef2cb0f5a472ae651c7bbf24cc7d578
Евгений Бондюк <evgenijbonduk@MacBook-Pro-Evgenij.local>
2022-02-20 17:16:19 +0300

    fix

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 require_once 'myHandler.php';
 ini_set('session.serialize_handler', 'php_serialize');
 $mySession = new myHandler();
-$maxlifetime = ini_get("session.gc_maxlifetime");
+$maxLife = ini_get("session.gc_maxlifetime");
 
 
 session_start();
 
-echo $maxlifetime;
-echo $cookielifetime;
+echo $maxLife;
+
 $_SESSION['name'] = 'Borya';
 $_SESSION['age'] = 25;
 
 session_write_close();
-session_destroy();
+
 
 // В своем примере я не использовал функцию  gc, так как в своем примере я изпользовал изменение в файле php.ini
 // так я поставил 86400
